b9754d6398e5fb639da9c3ca2aba6909a7f32f4f
J. Junior <junior.jaava@gmail.com>
2023-07-24 15:37:24 +0200

    OAuth 2.0 Resource Server - JWT Authentication Mechanism
    Signed-off-by: J. Junior <junior.jaava@gmail.com>

--- oauth2-resource-server/src/main/java/com/jj/oauth2/oauth2/resource/server/Oauth2ResourceServerApplication.java
+++ oauth2-resource-server/src/main/java/com/jj/oauth2/oauth2/resource/server/Oauth2ResourceServerApplication.java
@@ -3,6 +3,16 @@ package com.jj.oauth2.oauth2.resource.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.eclipse.microprofile.auth.LoginConfig;
+
+import javax.annotation.security.DeclareRoles;
+import javax.ws.rs.ApplicationPath;
+
+//To enable the JWT authentication mechanism in the server,
+//we need to add the LoginConfig annotation in the JAX-RS application:
+@ApplicationPath("/api")
+@DeclareRoles({"resource.read", "resource.write"})
+@LoginConfig(authMethod = "MP-JWT")
 @SpringBootApplication
 public class Oauth2ResourceServerApplication {
 
